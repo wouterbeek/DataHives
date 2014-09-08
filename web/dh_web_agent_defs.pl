e38acdea205fa20ec13d4b03162b23ab18c19215
Wouter Beek <me@wouterbeek.com>
2014-09-08 07:23:36 +0200

    Working on creation button.

--- web/dh_web_agent_defs.pl
+++ web/dh_web_agent_defs.pl
@@ -17,11 +17,13 @@ Web-based interface to agents in DataHives.
 :- use_module(library(aggregate)).
 :- use_module(library(http/html_head)).
 :- use_module(library(http/html_write)).
+:- use_module(library(http/http_cors)).
 :- use_module(library(http/http_dispatch)).
 :- use_module(library(http/http_json)).
 :- use_module(library(http/js_write)).
 
 :- use_module(generics(request_ext)).
+:- use_module(pl(pl_log)).
 
 :- use_module(dh_web(dh_web_generics)).
 
@@ -38,17 +40,19 @@ Web-based interface to agents in DataHives.
      ).
 :- endif.
 
-:- http_handler(dh_web(aliases), aliases, []).
-:- http_handler(dh_web(description), description, []).
+:- http_handler(dh_web(agentAliases), agentAliases, []).
+:- http_handler(dh_web(agentDescription), agentDescription, []).
+:- http_handler(dh_web(createAgent), createAgent, []).
 
 
 
-%! aliases(+Request:list(nvpair)) is det.
+%! agentAliases(+Request:list(nvpair)) is det.
 % Returns type/label pairs in JSON format.
 %
 % This can be used to populate a <select> element in HTML.
 
-aliases(_):-
+agentAliases(_):-
+  cors_enable,
   aggregate_all(
     set(Alias-Alias),
     dh:agent_definition(Alias, _),
@@ -58,7 +62,8 @@ aliases(_):-
   reply_json_dict(Dict).
 
 
-description(Request):-
+agentDescription(Request):-
+  cors_enable,
   request_query_nvpair(Request, alias, Alias),
   dh:agent_definition(Alias, Def),
   def_pairs(Def, Pairs0),
@@ -67,8 +72,11 @@ description(Request):-
   reply_json_dict(Dict).
 
 def_pairs([], []):- !.
-def_pairs([X-Y|T], [X-Y|T]):- !.
-def_pairs([H|T1], [H-""|T2]):-
+def_pairs([X1-Y|T1], [X2-Y|T2]):- !,
+  with_output_to(atom(X2), write_canonical_blobs(X1)),
+  def_pairs(T1, T2).
+def_pairs([H1|T1], [H2-""|T2]):-
+  with_output_to(atom(H2), write_canonical_blobs(H1)),
   def_pairs(T1, T2).
 
 
@@ -81,15 +89,16 @@ dh_web_agent_defs(_, HtmlStyle):-
 
 dh_agent_defs_body -->
   {
-    http_location_by_id(aliases, AliasesLocation),
-    http_location_by_id(description, DescriptionLocation)
+    http_location_by_id(agentAliases, AliasesLocation),
+    http_location_by_id(agentDescription, DescriptionLocation),
+    http_location_by_id(createAgent, CreateLocation)
   },
   html([
     \html_requires(js(jquery)),
     div(id=aliasesContainer, []),
     div(id=descriptionContainer, []),
     button(id=createBtn, ['Create agent']),
-    \js_script({|javascript(AliasesLocation,DescriptionLocation)||
+    \js_script({|javascript(AliasesLocation,DescriptionLocation,CreateLocation)||
 function changeDescription(data) {
   $("#createBtn").text("Create " + data["alias"] + " agent");
   $("#descriptionContainer").html(descriptionList(data["description"]));
@@ -120,13 +129,21 @@ $(document).ready(function() {
 });
 $("#aliasesContainer").on("change", "select", function() {
   $.ajax({
-    "data": {"alias": $(this).find("option:selected").text()},
+    "data": {"alias": $(this).find("option:selected").attr("value")},
     "dataType": "json",
     "fail": function(data) {
         console.log("Failed to change agent definition.", data);
       },
     "success": changeDescription,
     "url": DescriptionLocation
+  });
+});
+$("#createBtn").click(function() {
+  $.ajax({
+    "data": {"alias": $("#aliasesContainer option:selected").attr("value")},
+    "dataType": "json",
+    "fail": function(data) {console.log("Failed to create agent.", data);},
+    "url": CreateLocation
   });
 });
     |})
